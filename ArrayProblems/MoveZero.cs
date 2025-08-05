68f1f945647319ce733b5773e0986566ab3d7e10
admin42045 <nitinkumar42045@gmail.com>
2025-08-05 21:27:09 +0530

    array problem move zero

---
+++ ArrayProblems/MoveZero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ArrayProblems
+{
+    internal class MoveZero
+    {
+        internal int[] MoveZeros(int[] nums)
+            {
+                var basket = new List<int>();
+                foreach (int i in nums)
+                {
+                    if (i == 0)
+                    {
+                        basket.Add(i);
+                    }
+                }
+                foreach (int i in nums)
+                {
+                    if (i != 0)
+                    { basket.Add(i); }
+                }
+                return basket.ToArray();
+            }
+        }
+}
