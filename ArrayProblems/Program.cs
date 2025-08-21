3df3339d88c485b16ebb5b40cfb6bb8f2c6b0649
admin42045 <nitinkumar42045@gmail.com>
2025-08-21 11:12:57 +0530

    taken pull in office laptop

---
+++ ArrayProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 3, 12 };
+            var objMove = new MoveZero();
+            int[] result = objMove.MoveZeros(nums);
+            foreach (int i in result) 
+            {
+                Console.Write($"{i} ");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
