76efa7da4ac29c6a592bb7a6abc984e8aa60af6c
admin42045 <nitinkumar42045@gmail.com>
2025-08-22 08:27:43 +0530

    majority element problem solve

--- ArrayProblems/Program.cs
+++ ArrayProblems/Program.cs
@@ -6,13 +6,12 @@ namespace ArrayProblems
     {
         static void Main(string[] args)
         {
-            int[] nums = { 0, 1, 0, 3, 12 };
-            var objMove = new MoveZero();
-            int[] result = objMove.MoveZeros(nums);
-            foreach (int i in result) 
-            {
-                Console.Write($"{i} ");
-            }
+            int[] nums = { 2,2,2,2,3,3,3,3,3};
+            var objMove = new MajorityElement();
+            int result = objMove.MajorityElements(nums);
+
+            Console.Write($"{result} ");
+            
 
 
 
