03ef6832eb5cc9c9a041b40ce01936d08948a9f9
admin42045 <nitinkumar42045@gmail.com>
2025-08-24 22:54:58 +0530

    c# 7 tuples examples

---
+++ C#_7_Tuples/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+/*
+============ Tuples ==============
+Why do we need tuples?
+Ans -> If you want to return more than one value from a method then you need to use tuples c#. It is common  thing to return multiple values from a method.
+    Tuples in c# 7 provide a better machenicsem to return multiple values from a method.
+
+What are the differencet ways to return more than one value from a method?
+Ans -> 
+a) Using Custom Data Type: you can return multiple value from the method by using a custom data type as the return type of the method. 
+    but sometimes we don't need want to use classes and objects because that's just too much for the given purpose.
+
+b) Using Ref and Out Variable: you can also return the more than one value from the method either by using the out and ref parameters.
+c) Using dynamic keyword: you can retrun more tha one value from a method by using the dynamic keyword as the return type. The dynamic keyword was introducted in c# 4.
+
+
+
+
+Explaines of Tuples Before c# 7?
+
+Understanding the problems with the tuples before c# 7?
+
+How do we use Tuples from c# 7?
+
+Tuples in c# with named parameters?
+
+Guidlines to use Tuples?
+
+*/
+namespace C__7_Tuples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var values1 = new List<double>() { 10, 20, 30, 40, 50 };
+            var values2 = new List<double>() { 10, 20, 30, 40, 50, 60 };
+            var values3 = new List<double>() { 10, 20, 30, 40, 50, 60, 70, 80, 90 ,100};
+            // store the result of calculate method in a variable of tuple type 
+            Tuple<int, double> result  = Calculate1(values1);
+            var results2 = Calculate2(values2);
+            var (countResult, sumResult) = Calulate3(values3);
+            // access the first value using item1 and second value using item2 properties
+            Console.WriteLine($"There are {result.Item1} values and their sum is {result.Item2}");
+            Console.WriteLine($"There are {results2.Item1} values and their sum is {results2.Item2}");
+            Console.WriteLine($"There are {countResult} values and their sum is {sumResult}");
+
+            Console.ReadKey();
+
+        }
+        public static Tuple<int, double> Calculate1(IEnumerable<double> values)
+        {
+            int count = 0;
+            double sum = 0.0;
+            foreach(var value in values)
+            {
+                count++;
+                sum += value;
+            }
+            Tuple<int, double> t = Tuple.Create(count, sum);
+
+            return t;
+        }
+
+        public static (int , double ) Calculate2(IEnumerable<double> values)
+        {
+            int count = 0;
+            double sum = 0.0; 
+            foreach(var value in values)
+            {
+                count++;
+                sum += value;
+            }
+            return (count, sum);
+        }
+
+        private static (int count, double sum) Calulate3(IEnumerable<double> values)
+        {
+            int count = 0;
+            double sum = 0.0;
+            foreach(var value in values)
+            {
+                count++;
+                sum += value;
+            }
+            return (count, sum);
+        }
+
+        // Splitting the tuples in c# 7
+        
+    }
+}
---
+++ SplitTuples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SplitTuples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var EmployeeDetails = GetEmployeeDetails(1001);
+
+            var Name = EmployeeDetails.Item1;
+            var Salary = EmployeeDetails.Item2;
+            var Gender = EmployeeDetails.Item3;
+            var Department = EmployeeDetails.Item4;
+
+            Console.WriteLine($"{Name} {Salary} {Gender} {Department}");
+            Console.WriteLine($"Press any key to exit!");
+
+            // Method 1: De-constructing the tuples
+            (string name, double salary, string gender, string department) = GetEmployeeDetails(10001);
+            // or
+            var (name1, salary1, gender1, department1) = GetEmployeeDetails(1001);
+
+
+            
+            Console.ReadKey();
+        }
+        public static (string, double, string, string) GetEmployeeDetails(long EmployeeID)
+        {
+            string EmployeeName = "Nitin";
+            double Salary = 2000;
+            string Gender = "Male";
+            string Department = "IT";
+            return (EmployeeName, Salary, Gender, Department);
+        }
+    }
+}
